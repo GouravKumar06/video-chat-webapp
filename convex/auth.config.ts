6678fa35c21c54bbc3768b0f55d7c23af300afca
GouravKumar06 <gka98963@gmail.com>
2025-05-24 20:00:40 +0530

    webhook implementation

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+const authConfig = {
+    providers: [
+      {
+        domain: 'https://knowing-dragon-40.clerk.accounts.dev',
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig;
+  
