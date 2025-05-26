7b050c3b9fd2fe376e7812ee77936902680e35e3
GouravKumar06 <gka98963@gmail.com>
2025-05-26 21:28:27 +0530

    add video calling functionality

--- src/components/home/chat-bubble.tsx
+++ src/components/home/chat-bubble.tsx
@@ -50,7 +50,7 @@ const ChatBubble = ({ me, message, previousMessage }: ChatBubbleProps) => {
 				<DateIndicator message={message} previousMessage={previousMessage} />
 				<div className='flex gap-1 w-2/3'>
 					<ChatBubbleAvatar isGroup={isGroup} isMember={isMember} message={message} fromAI={fromAI} />
-					<div className={`flex flex-col z-20 max-w-fit px-2 pt-1 rounded-md shadow-md relative ${bgClass}`}>
+					<div className={`flex flex-col z-20 max-w-fit px-2 pt-1 pb-2 rounded-md shadow-md relative ${bgClass}`}>
 						{!fromAI && <OtherMessageIndicator />}
 						{fromAI && <Bot size={16} className='absolute bottom-[2px] left-2' />}
 						{<ChatAvatarActions message={message} me={me} />}
@@ -68,7 +68,7 @@ const ChatBubble = ({ me, message, previousMessage }: ChatBubbleProps) => {
 			<DateIndicator message={message} previousMessage={previousMessage} />
 
 			<div className='flex gap-1 w-2/3 ml-auto'>
-				<div className={`flex  z-20 max-w-fit px-2 pt-1 rounded-md shadow-md ml-auto relative ${bgClass}`}>
+				<div className={`flex  z-20 max-w-fit px-2 pt-1 pb-2 rounded-md shadow-md ml-auto relative ${bgClass}`}>
 					<SelfMessageIndicator />
 					{renderMessageContent()}
 					{open && <ImageDialog src={message.content} open={open} onClose={() => setOpen(false)} />}
